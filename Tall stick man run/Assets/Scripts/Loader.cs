0997f424da3cca153d24db1fcac93d7a67a4f058
citrongames <55977306+citrongames@users.noreply.github.com>
2022-06-04 19:00:47 +0600

    Main
    General add

---
+++ Tall stick man run/Assets/Scripts/Loader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    [SerializeField] private int _levelIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_levelIndex > 0)
+            StartCoroutine(LoadLevel(_levelIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
